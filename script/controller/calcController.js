c232d6f7893ba8137c588abb0b2bf5b5db4562bb
mainardes1 <allanmainardes@live.com>
2020-02-14 00:33:17 -0200

    adequação do código p/ ingles e criação do initialize

--- script/controller/calcController.js
+++ script/controller/calcController.js
@@ -1,5 +1,98 @@
 class CalcController{
 
+        constructor(){
+            //_ antes do atributo quer dizer que o atributo é privado.
+            this._locale = "pt-br";
+            this._displayCalcEL = document.querySelector("#display");
+            this._dateEL = document.querySelector("#data");
+            this._timeEL = document.querySelector("#hora");
+            
+            this._currentDate;
+            this.initialize();
+        }
+
+        initialize(){
+
+            this.setDisplayDateTime(); // para executar imediatamente ao abrir a calculadora
+
+            setInterval(()=>{
+
+                this.setDisplayDateTime();
+
+            }, 1000);
+
+            /*setTimeout (()=>{ 
+
+                clearInterval(interval);     //executar após 10 segundos, ou seja dps de 10 segundos vai parar a função de cima
+
+            }, 10000);*/
+        }
+
+        setDisplayDateTime(){
+
+            this.displayDate = this.currentDate.toLocaleDateString(this.locale); //, day='2-digit', month='short', year='numeric' p/ data extensa); // tudo o que for repetido criar método;
+            this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
+
+        }
+
+        get displayTime(){
+
+            return this._timeEL.innerHTML;
+
+        }
+
+        set displayTime(value){
+
+            return this._timeEL.innerHTML = value;
+
+        }
+
+        get displayDate(){
+
+            return this._dateEL.innerHTML;
+
+        }
+
+        set displayDate(value){
+
+            return this._dateEL.innerHTML = value;
+
+        }
+
+
+
+        get displayCalc(){
+
+            return this._displayCalcEL.innerHTML;
+        
+        }
+
+        set displayCalc(valor){
+
+            this._displayCalcEL.innerHTML = valor;
+
+        }
+
+        get currentDate(){
+
+            return new Date();
+
+        }
+
+        set currentDate(data){
+
+            this._currentDate = data;
+        }
+
+        get currentTime(){
+
+            return this._currentTime;
+        }
+
+        set currentTime(hour){
+
+            this._currentTime = hour;
+        }
     
 
 }
