9fb6b33e170002fdc9097d7bc05e49e912541568
mainardes1 <allanmainardes@live.com>
2020-04-09 10:27:44 -0300

    alteração switch case

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,7 @@ class CalcController{
 
     constructor(){
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -38,6 +39,83 @@ class CalcController{
 
         });
 
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+
+        switch (value){
+        
+            case 'ac':
+                this.clearAll();
+            break;
+
+            case 'ce':
+
+                this.cancelEntry();
+
+            break;
+
+            case 'soma':
+
+            break;
+
+            case 'subtracao':
+
+            break;
+            
+            case 'multiplicacao':
+
+            break;
+
+            case 'divisao':
+
+            break;
+
+            case 'porcento':
+
+            break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+            break;
+
+        }
     }
 
     initButtonsEvents(){
